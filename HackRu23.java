a407b5a355ba2c4a09060bf376f96521cf260253
Nico <nicholas.panlilio@rutgers.edu>
2023-10-07 13:23:04 -0400

    nico change

--- HackRu23.java
+++ HackRu23.java
@@ -2,6 +2,6 @@ public class HackRu23
 {
     public static void main(String[] args)
     {
-        System.out.println("ysa says HELLO in main ");
+        System.out.println("NICO says HELLO in main ");
     }
 }
